cc82cd6d8a92ca893e15ea2fef208f5a95c71886
Stephen Price <stephen@stp5.net>
2018-04-14 14:22:13 -0700

    run command

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+set -e
+
 sed -i "s/%%SERVER_NAMES%%/${SERVER_NAMES}/" dnscrypt-proxy.toml
 sed -i "s/%%REQUIRE_DNSSEC%%/${REQUIRE_DNSSEC}/" dnscrypt-proxy.toml
 sed -i "s/%%LOG_LEVEL%%/${LOG_LEVEL}/" dnscrypt-proxy.toml
+
+./dnscrypt-proxy
